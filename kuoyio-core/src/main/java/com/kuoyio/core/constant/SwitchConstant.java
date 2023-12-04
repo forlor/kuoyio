8bdd5e5710c5faa755654cfb3a860b15147d8c25
xuyuzhuang <forlor@live.com>
2023-12-04 22:39:51 +0800

    init

---
+++ kuoyio-core/src/main/java/com/kuoyio/core/constant/SwitchConstant.java
@@ -0,0 +1,15 @@
+package com.kuoyio.core.constant;
+
+public final class SwitchConstant {
+    public static final boolean ENABLE = Boolean.TRUE;
+
+    public static final boolean DISABLE = Boolean.FALSE;
+    public static final int OPEN = 1;
+    public static final int CLOSE = 0;
+    public static final String OPEN_STR = "1";
+    public static final String CLOSE_STR = "0";
+
+    private SwitchConstant() {
+    }
+
+}
