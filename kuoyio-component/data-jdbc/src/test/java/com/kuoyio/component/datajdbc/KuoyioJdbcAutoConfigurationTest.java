df071316f663eb6b3e3e2506c0f0be46721dc56f
xuyuzhuang <xuyuzhuang@bjhy-xt.com>
2023-12-06 17:48:57 +0800

    springboot test

---
+++ kuoyio-component/data-jdbc/src/test/java/com/kuoyio/component/datajdbc/KuoyioJdbcAutoConfigurationTest.java
@@ -0,0 +1,18 @@
+package com.kuoyio.component.datajdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author xyz
+ * @since 1.0
+ */
+@DataJdbcTest
+public class KuoyioJdbcAutoConfigurationTest {
+
+    @Test
+    public void test(){
+
+    }
+}
---
+++ kuoyio-component/data-jdbc/src/test/java/com/kuoyio/component/datajdbc/SpringDataJdbcStart.java
@@ -0,0 +1,11 @@
+package com.kuoyio.component.datajdbc;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author xyz
+ * @since 1.0
+ */
+@SpringBootTest
+public class SpringDataJdbcStart {
+}
